1b3e03e2a4cb901dc6902a3d572c188458932da3
Tomas Nagy <Tomas@Tomass-MacBook-Pro.local>
2014-09-23 10:49:57 +0200

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Tomas on 9/23/14.
+ */
+document.addEventListener("DOMContentLoaded", function() {
+    var name = document.getElementById("name");
+    var lastname = document.getElementById("lastname");
+
+    name.style.border = "solid 1px #f00";
+    lastname.style.border = "solid 1px #f00";
+});
